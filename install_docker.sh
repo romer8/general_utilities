5d580e770b3bf096c3c028f23f2efe9ff5cf4eb2
romer8 <gio.busrom@gmail.com>
2022-01-12 11:30:39 -0700

    Addition of the file to install docker and docker-compose with just one command

---
+++ install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing Docker"
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
+
+echo "Installing Docker Compose"
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+echo "Done"
