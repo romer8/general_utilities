01b45c6160d5f404cef80888d4774eb442227001
romer8 <gio.busrom@gmail.com>
2022-01-19 12:48:41 -0700

    adding upgrade

--- install_docker.sh
+++ install_docker.sh
@@ -13,6 +13,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
