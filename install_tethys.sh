9c066b3220b2f598cdb7d708d696b38a65556046
romer8 <gio.busrom@gmail.com>
2022-01-20 11:40:26 -0700

    working tethys installation

--- install_tethys.sh
+++ install_tethys.sh
@@ -4,10 +4,6 @@ echo "Starting Tethys Installation"
 
 ~/miniconda3/bin/conda create -n tethys -c tethysplatform -c conda-forge tethys-platform
 
-~/miniconda3/bin/activate tethys
-
-tethys gen portal_config
-
-tethys db configure
+source ~/miniconda3/bin/activate tethys && tethys gen portal_config && tethys db configure
 
 echo "Finished Tethys Installation"
